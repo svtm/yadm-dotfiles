d7b5bc8193b1bf9a96cec048c567a02d6924c93a
Eirik Nielsen <eirik.nielsen@signicat.com>
2019-11-14 10:12:33 +0100

    add powerline10k, change font

--- scripts/bus.py
+++ scripts/bus.py
@@ -5,58 +5,68 @@ import json
 import dateutil.parser
 from datetime import datetime
 
-client = GraphQLClient('https://api.entur.io/journey-planner/v2/graphql')
-client.inject_token("enielsen - personaldashboard", "ET-Client-Name")
+def make_request():
+  client = GraphQLClient('https://api.entur.io/journey-planner/v2/graphql')
+  client.inject_token("enielsen - personaldashboard", "ET-Client-Name")
 
-res_string = client.execute('''
-{
-  trip(
-    from: {
-      place: "NSR:StopPlace:59977"
-    }, 
-    to: {
-      place: "NSR:StopPlace:44096"
-    }, 
-    numTripPatterns: 3, 
-  ) {
-    tripPatterns {
-      startTime
-      duration
-      legs {
-        line {
-          id
-          publicCode
-          authority {
-            name
+  return client.execute('''
+  {
+    trip(
+      from: {
+        place: "NSR:StopPlace:59977"
+      }, 
+      to: {
+        place: "NSR:StopPlace:44096"
+      }, 
+      numTripPatterns: 3, 
+    ) {
+      tripPatterns {
+        startTime
+        duration
+        legs {
+          line {
+            id
+            publicCode
+            authority {
+              name
+            }
           }
-        }
-        fromEstimatedCall {
-          realtime
-          aimedDepartureTime
-          expectedDepartureTime
-          actualDepartureTime
+          fromEstimatedCall {
+            realtime
+            aimedDepartureTime
+            expectedDepartureTime
+            actualDepartureTime
+          }
         }
       }
     }
   }
-}
-''')
+  ''')
 
-trips = json.loads(res_string)["data"]["trip"]["tripPatterns"]
+def extract_trips(res_string):
+  trips = json.loads(res_string)["data"]["trip"]["tripPatterns"]
+  return trips
 
-time_strings = []
+def pretty_print(trips):
+  time_strings = []
 
-for trip in trips:
-    expectedTime = trip["legs"][0]["fromEstimatedCall"]["expectedDepartureTime"]
-    expectedTime = dateutil.parser.parse(expectedTime)
-    realtime = trip["legs"][0]["fromEstimatedCall"]["realtime"]
+  for trip in trips:
+      expectedTime = trip["legs"][0]["fromEstimatedCall"]["expectedDepartureTime"]
+      expectedTime = dateutil.parser.parse(expectedTime)
+      realtime = trip["legs"][0]["fromEstimatedCall"]["realtime"]
 
-    aimedTime = trip["legs"][0]["fromEstimatedCall"]["aimedDepartureTime"]
-    aimedTime = dateutil.parser.parse(aimedTime)
+      aimedTime = trip["legs"][0]["fromEstimatedCall"]["aimedDepartureTime"]
+      aimedTime = dateutil.parser.parse(aimedTime)
 
-    if realtime and (expectedTime - aimedTime).total_seconds() > 60:
-        time_strings.append(f"{expectedTime.hour}:{expectedTime.minute:02d} ({aimedTime.hour}:{aimedTime.minute:02d})")
-    else:
-        time_strings.append(f"{expectedTime.hour}:{expectedTime.minute:02d}")
+      if realtime and (expectedTime - aimedTime).total_seconds() > 60:
+          time_strings.append(f"{expectedTime.hour}:{expectedTime.minute:02d} ({aimedTime.hour}:{aimedTime.minute:02d})")
+      else:
+          time_strings.append(f"{expectedTime.hour}:{expectedTime.minute:02d}")
 
-print(" | ".join(time_strings))
+  print(" | ".join(time_strings))
+
+
+if __name__ == "__main__":
+  res_string = make_request()
+  trips = extract_trips(res_string)
+  pretty_print(trips)
